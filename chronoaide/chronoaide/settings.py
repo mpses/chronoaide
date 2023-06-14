e1ba54fc32021c9663e2a2a3f58de8f52e8bb71c
23taktstock <nagura-takuto@g.ecc.u-tokyo.ac.jp>
2023-06-14 18:04:09 +0900

    Add TextToScheduleConfig to settings.py

--- chronoaide/chronoaide/settings.py
+++ chronoaide/chronoaide/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'text_to_schedule.apps.TextToScheduleConfig',
 ]
 
 MIDDLEWARE = [
