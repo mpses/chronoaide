8594e6211c6ff7fd9510431c6f6f7f8592f4fa45
nagutaku <n.taku.tomato@gmail.com>
2023-07-04 23:26:57 +0900

    Make format_event_data callable via api

--- chronoaide/api/views.py
+++ chronoaide/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # import from parent directory
 import sys
@@ -10,5 +12,24 @@ from text_to_schedule import views as tts_views
 def index(request):
     pass
 
+@api_view(['POST'])
 def textToSchedule(request):
-    return tts_views.format_event_data(request.POST['text'])
+    raw_text = request.data['text']
+    # schedule = tts_views.format_event_data(raw_text)
+    # for example
+    schedule = [
+        {
+            'name': '太郎君誕生日会',
+            'start': '2023-04-24 10:00',
+            'end': None,
+            'place': '代々木公園',
+            'detail': 'プレゼントを持ってくること。'
+        },{
+            'name': '東京大学入学式',
+            'start': '2023-04-03 10:00',
+            'end': '2023-04-03 12:00',
+            'place': '日本武道館',
+            'detail': '入学式のため、スーツを着ていくこと。'
+        }
+    ]
+    return Response(schedule)
