333fb3a76c658e9d5ec72e5e67b98e6a7d4e9005
nagutaku <n.taku.tomato@gmail.com>
2023-07-04 23:45:10 +0900

    Make add_to_google_calendar callable via api

--- chronoaide/api/urls.py
+++ chronoaide/api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('text_to_schedule/', views.text_formatter_api, name = 'text_to_schedule'),
+    path('add_to_google_calendar/', views.add_to_google_calendar_api, name = 'add_to_google_calendar'),
 ]
--- chronoaide/api/views.py
+++ chronoaide/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 # import from parent directory
 import sys
@@ -17,7 +18,7 @@ def text_formatter_api(request):
     raw_text = request.data['text']
     # schedule = tts_views.format_event_data(raw_text)
     # for example
-    schedule = [
+    schedule = {'schedules':[
         {
             'name': '太郎君誕生日会',
             'start': '2023-04-24 10:00',
@@ -31,5 +32,15 @@ def text_formatter_api(request):
             'place': '日本武道館',
             'detail': '入学式のため、スーツを着ていくこと。'
         }
-    ]
+    ]}
     return Response(schedule)
+
+@api_view(['POST'])
+def add_to_google_calendar_api(request):
+    schedules = request.data['schedules']
+    token = request.data['token']
+    try:
+        tts_views.add_to_google_calendar(schedules)
+        return Response({'message': 'success'}, status = status.HTTP_200_OK)
+    except Exception as e:
+        return Response({'message': str(e)}, status = status.HTTP_400_BAD_REQUEST)
